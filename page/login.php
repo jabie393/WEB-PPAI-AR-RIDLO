ce63ce94eaeb726c378d0ccc55e52d067db8562e
jabie393 <wahyurojabi393@gmail.com>
2025-01-24 09:33:16 +0700

    ui backend

--- page/login.php
+++ page/login.php
@@ -41,6 +41,36 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             height: auto;
             margin: 0 auto;
         }
+        /* Tambahkan gaya khusus untuk field form */
+        .form-control {
+            border-color: #28a745; /* Warna hijau */
+            background-color: green!important;
+        }
+        .form-control:focus {
+            background-color: #c3e6cb!important; /* Warna hijau lebih cerah saat fokus */
+            border-color: #28a745; /* Warna hijau saat fokus */
+            box-shadow: 0 0 5px rgba(40, 167, 69, 0.5); /* Efek cahaya hijau */
+        }
+        .btn {
+            color: white;
+            background-color: green;
+            border-color: green;
+        }
+        .btn:hover {
+            color: white;
+            background-color: #B8B6B6;
+            border-color: #B8B6B6;
+        }
+        .btn:after {
+            color: white;
+            background-color: #B8B6B6!important;
+            border-color: #B8B6B6!important;
+        }
+        .btn:focus {
+            box-shadow: none;
+            border: none;
+            outline: none;
+        }
     </style>
 </head>
 <body>
@@ -49,9 +79,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <div class="col-md-4 mt-5">
                 <div class="card">
                     <div class="card-body">
-                    <div class="logo-container">
-                        <img src="..\assets\logo\logo.png" class="logo" alt="logo">
-                    </div>
+                        <div class="logo-container">
+                            <img src="..\assets\logo\logo.png" class="logo" alt="logo">
+                        </div>
                         <h2 class="text-center">Login Admin</h2>
                         <form action="login.php" method="POST">
                             <div class="form-group">
@@ -62,7 +92,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                                 <label for="password">Password:</label>
                                 <input type="password" id="password" name="password" class="form-control" required>
                             </div>
-                            <button type="submit" class="btn btn-primary btn-block">Login</button>
+                            <button type="submit" class="btn btn-block">Login</button>
                         </form>
                         <?php if (isset($_POST['username']) && isset($_POST['password'])) : ?>
                             <div class="alert alert-danger mt-3">
